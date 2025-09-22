199256424408bed0c35856343b7ddeb727590627
erendikmen <dikmeneren@hotmail.com>
2025-09-22 21:59:24 +0300

    Day 5: Add solution for HackerRank problems

---
+++ HackerRank/2025-09-22/Finding-the-percentage.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    toplam = 0
+    for i in student_marks[query_name]:
+        toplam += i
+    print(f"{(toplam/3):.2f}") # Sonuç 2 basamaklı bir float olur
